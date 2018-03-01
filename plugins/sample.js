55853c3a4bf741809d2ff8d624b527d044011492
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-03-01 16:30:06 -0500

    Update sample.js

--- plugins/sample.js
+++ plugins/sample.js
@@ -11,6 +11,16 @@ module.exports = {
   meta: {
     name: 'Sample Plugin',
     version: '1.0.0',
-    description: 'Sample Plugin With Custom Skull and Argruments Test Commands'
+    description: 'Sample Plugin With Custom Skull and Argruments Test Commands',
+    commands: {
+      skull: {
+        args: '<Skull Owner>',
+        description: 'Give The Caller A Skull With The Set Skull Owner'
+      },
+      args: {
+        args: '<Argruments>',
+        description: 'Prints A JSON Array Of All Argruments'
+      }
+    }
   }
 };
