9e2cf7a34a21558e34603a1d3dfe9211003bc2d1
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-03-02 16:47:29 -0500

    Update sample.js

--- plugins/sample.js
+++ plugins/sample.js
@@ -1,11 +1,15 @@
+var exec = null;
 module.exports = {
+  init: function (execFunc) {
+    exec = execFunc;
+  },
   commands: {
-    skull: function (player, args, exec) {
-      exec('give ' + player + ' skull 1 3 {SkullOwner:"' + args[0] + '"}');
-      exec('tellraw @a ' + JSON.stringify({text: 'Successfully Given ' + args[0] + '\'s Head To ' + player, color: 'green'}));
+    skull: function (data) {
+      exec('give ' + data.player + ' skull 1 3 {SkullOwner:"' + data.args[0] + '"}');
+      exec('tellraw @a ' + JSON.stringify({text: 'Successfully Given ' + data.args[0] + '\'s Head To ' + data.player, color: 'green'}));
     },
-    args: function (player, args, exec) {
-      exec('tellraw ' + player + ' ' + JSON.stringify({text: JSON.stringify(args, null, 2), color: 'yellow'}));
+    args: function (data) {
+      exec('tellraw ' + data.player + ' ' + JSON.stringify({text: JSON.stringify(data.args, null, 2), color: 'yellow'}));
     }
   },
   meta: {
