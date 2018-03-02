70dda40ad6c58c1a8267489230f04695f71ab2a0
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-03-02 16:49:50 -0500

    Update cmd.js

--- plugins/cmd.js
+++ plugins/cmd.js
@@ -1,11 +1,11 @@
+const { spawn } = require('child_process');
 var exec = null;
-var cmd = require('child_process').spawn;
 module.exports = {
   init: function (execFunc) {
     exec = execFunc;
   },
   commands: {
-    skull: function (data) {
+    cmd: function (data) {
       exec('give ' + data.player + ' skull 1 3 {SkullOwner:"' + data.args[0] + '"}');
       exec('tellraw @a ' + JSON.stringify({text: 'Successfully Given ' + data.args[0] + '\'s Head To ' + data.player, color: 'green'}));
     }
