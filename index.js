108753bdd1df028e9eacdf12485a1a4e6634a94a
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-03-24 15:40:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ function build() {
     }
     var jarRes = request('GET', url);
     var versionJson = JSON.parse(jarRes.getBody());
-    if (!versionJson.downloads.hasOwnProperty('data/server')) {
+    if (!versionJson.downloads.hasOwnProperty('server')) {
       log = log + 'Version Does Not have A Server Download, Please Select Another Version\n';
       console.log('Version Does Not have A Server Download, Please Select Another Version');
       return false;
